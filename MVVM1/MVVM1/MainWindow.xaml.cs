72ab443a44984f823259513d961ad850eb58ef7e
dusanbe <dusanbiocanin@hotmail.com>
2017-03-30 16:31:53 +0200

    BuildOne

--- MVVM1/MVVM1/MainWindow.xaml.cs
+++ MVVM1/MVVM1/MainWindow.xaml.cs
@@ -12,17 +12,47 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MVVM1.Models;
+using System.Collections.ObjectModel;
 
 namespace MVVM1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
     public partial class MainWindow : Window
     {
+        readonly ObservableCollection<Inventory> _cars;
         public MainWindow()
         {
             InitializeComponent();
+            _cars = new ObservableCollection<Inventory>
+            {
+                new Inventory {CarId=1,Color="Blue",Make="Chevy",PetName="Kit", IsChanged=false },
+                new Inventory {CarId=2,Color="Red",Make="Ford",PetName="Red Rider", IsChanged=false },
+            };
+            cboCars.ItemsSource = _cars;
+        }
+
+        private void btnAddCar_Click(object sender, RoutedEventArgs e)
+        {
+            var maxCount = _cars?.Max(x => x.CarId) ?? 0;
+            _cars?.Add(new Inventory { CarId = ++maxCount, Color = "Yellow", Make = "VW", PetName = "Birdie" });
+        }
+
+        private void btnChangeColor_Click(object sender, RoutedEventArgs e)
+        {
+            var car = _cars.FirstOrDefault(x => x.CarId == ((Inventory)cboCars.SelectedItem)?.CarId);
+            if (car != null)
+            {
+                car.Color = "Pink";
+            }
+        }
+
+        private void btnRemoveCar_Click(object sender, RoutedEventArgs e)
+        {
+            _cars.RemoveAt(0);
         }
     }
 }
---
+++ MVVM1/MVVM1/Models/Inventory.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM1.Models
+{
+    public class Inventory : INotifyPropertyChanged
+    {
+        private int _carId;
+        public int CarId
+        {
+            get { return _carId; }
+            set
+            {
+                if (value == _carId) return;
+                _carId = value;
+                OnPropertyChanged();
+            }
+        }
+        private string _make;
+        public string Make
+        {
+            get { return _make; }
+            set
+            {
+                if (value == _make) return;
+                _make = value;
+                OnPropertyChanged();
+            }
+        }
+        private string _color;
+        public string Color
+        {
+            get { return _color; }
+            set
+            {
+                if (value == _color) return;
+                _color = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _petName;
+        public string PetName
+        {
+            get { return _petName; }
+            set
+            {
+                if (value == _petName) return;
+                _petName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _isChanged;
+        public bool IsChanged
+        {
+            get { return _isChanged; }
+            set
+            {
+                if (value == _isChanged) return;
+                _isChanged = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        internal void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (propertyName != nameof(IsChanged))
+            {
+                IsChanged = true;
+            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
